0b2e2a31fdde4354f8ad67e2d8472319a91e47e1
igelithromada <pesakradek@seznam.cz>
2025-07-24 20:06:53 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 // app/layout.tsx
 import "./globals.css";
+import { ReactNode } from "react";
 
 export const metadata = {
   title: "Dolní Lhota v pohybu",
   description: "Běžecká výzva Dolní Lhota"
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs">
       <body>
