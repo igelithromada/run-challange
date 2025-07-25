12457b56c76c89e3e545e9e5ce8ec3ce1ff91b4a
igelithromada <pesakradek@seznam.cz>
2025-07-25 18:45:00 +0200

    Update page.tsx

--- app/myruns/page.tsx
+++ app/myruns/page.tsx
@@ -144,10 +144,10 @@ export default function MyRunsPage() {
     } else router.push("/" + item);
   };
   
-  return (
-    <>
-      <Navbar onMenuClick={() => setMenuVisible(true)} onHomeClick={() => router.push("/")} />
-      <Sidebar visible={menuVisible} onClose={() => setMenuVisible(false)} onSelect={handleSelect} />
+    return (
+      <>
+        <Navbar onMenuClick={() => setMenuVisible(true)} onHomeClick={() => router.push("/")} />
+        <Sidebar visible={menuVisible} onClose={() => setMenuVisible(false)} onSelect={handleSelect} />
 
     <div className="container">
       <h1 className="centered-title">Moje aktivity</h1>
