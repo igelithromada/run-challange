27245912a199eb992a4ac9cd6701d99fab9eff13
igelithromada <pesakradek@seznam.cz>
2025-08-05 00:26:45 +0200

    Update page.tsx

--- app/admin/[id]/records/page.tsx
+++ app/admin/[id]/records/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { db, storage } from "@/lib/firebase";
+import { db, storage } from "../../../lib/firebase";
 import {
   collection,
   query,
