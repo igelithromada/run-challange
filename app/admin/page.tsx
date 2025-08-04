8e3bd1bf8a9e2157d6787fac9c1484327e04a796
igelithromada <pesakradek@seznam.cz>
2025-08-04 06:56:16 +0200

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div className="container">
+      <h1 className="centered-title">Admin panel (dočasně pro všechny)</h1>
+      <p>Sem přidáme administraci.</p>
+    </div>
+  );
+}
